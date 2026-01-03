39470f516837b9f1931b8bf16fc7809927073553
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-02 23:36:01 -0300

    Create locales.nix

---
+++ nixos/modules/locales.nix
@@ -0,0 +1,21 @@
+i18n.defaultLocale = "pt_BR.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+  };
+
+  # Console e teclado (útil para tty)
+  console = {
+    font = pkgs.terminus_font;
+    keyMap = "br-abnt2";
+  };
+
+  # Layout de teclado no X11/Wayland
+  services.xserver.xkb.layout = "br";
